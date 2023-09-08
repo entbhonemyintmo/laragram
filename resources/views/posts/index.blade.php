bb1f1ada9af421ac2a890de4fa62ad0286b8549c
entbhonemyintmo <entbhonemyintmo@gmail.com>
2023-09-08 23:15:17 +0630

    feat: pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,20 +4,26 @@
     <div class="container">
         @foreach ($posts as $post)
             <div class="row">
-                <div class="col-6 offset-2">
+                <div class="col-6 offset-3">
                     <img src="/storage/{{ $post->media }}"
                          alt=""
                          class="w-100">
                 </div>
             </div>
-            <div class="row">
-                <div class="col-6 offset-2">
-                    <h3>{{ auth()->user()->username }}
+            <div class="row ">
+                <div class="col-6 offset-3">
+                    <h3>{{ $post->user->name }}
                     </h3>
                     <p>{{ $post->caption }}
                     </p>
                 </div>
             </div>
         @endforeach
+
+
+
+        {{ $posts->links('pagination::bootstrap-5') }}
+
+
     </div>
 @endsection
